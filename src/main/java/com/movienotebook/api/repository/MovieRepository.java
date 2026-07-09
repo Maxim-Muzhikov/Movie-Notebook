9f22db5ae83882c1bb7fb0db24edd1d1fad1ff13
mashaserova <muzikov.maxim@gmail.com>
2026-07-09 13:28:52 +0400

    Add pageable for search in repository

--- src/main/java/com/movienotebook/api/repository/MovieRepository.java
+++ src/main/java/com/movienotebook/api/repository/MovieRepository.java
@@ -1,6 +1,8 @@
 package com.movienotebook.api.repository;
 
 import com.movienotebook.api.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 	
-	List<Movie> findAllByTitleContainingIgnoreCaseOrOriginalTitleContainingIgnoreCase(String title, String originalTitle);
+	Page<Movie> findAllByTitleContainingIgnoreCaseOrOriginalTitleContainingIgnoreCase(String title, String originalTitle, Pageable pageable);
 	
 	List<Movie> findAllByTitleContainingIgnoreCase(String title);
 	
