c37b67c6fd9d4ae3758e4dc841422306b5c728ae
Maxim-Muzhikov <muzikov.maxim@gmail.com>
2026-07-10 13:29:56 +0400

    Add pagintation util

---
+++ src/main/java/com/movienotebook/api/util/PaginationCalculator.java
@@ -0,0 +1,38 @@
+package com.movienotebook.api.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaginationCalculator {
+	
+	private static final int EXTERNAL_PAGE_SIZE = 20;
+	
+	public record PaginationMapping(
+			List<Integer> externalPagesToFetch,
+			int relativeStartIndex,
+			int relativeEndIndex
+	) {}
+	
+	public static PaginationMapping calculate(int requestedPage, int requestedSize) {
+		if (requestedPage < 1 || requestedSize < 1) {
+			throw new IllegalArgumentException("Страница и размер должны быть больше 0");
+		}
+		
+		int absoluteStartIndex = (requestedPage - 1) * requestedSize;
+		int absoluteEndIndex = absoluteStartIndex + requestedSize - 1;
+		
+		int startExternalPage = (absoluteStartIndex / EXTERNAL_PAGE_SIZE) + 1;
+		int endExternalPage = (absoluteEndIndex / EXTERNAL_PAGE_SIZE) + 1;
+		
+		List<Integer> pagesToFetch = new ArrayList<>();
+		for (int i = startExternalPage; i <= endExternalPage; i++) {
+			pagesToFetch.add(i);
+		}
+		
+		int firstFetchedItemAbsoluteIndex = (startExternalPage - 1) * EXTERNAL_PAGE_SIZE;
+		int relativeStartIndex = absoluteStartIndex - firstFetchedItemAbsoluteIndex;
+		int relativeEndIndex = relativeStartIndex + requestedSize;
+		
+		return new PaginationMapping(pagesToFetch, relativeStartIndex, relativeEndIndex);
+	}
+}
