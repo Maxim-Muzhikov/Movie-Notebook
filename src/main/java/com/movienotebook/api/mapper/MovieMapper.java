acebf9d5e8597fe3a52d5a8c57ae3e5f750acebc
Maxim-Muzhikov <muzikov.maxim@gmail.com>
2026-07-13 14:59:00 +0400

    Add update method in mapper for MovieService

--- src/main/java/com/movienotebook/api/mapper/MovieMapper.java
+++ src/main/java/com/movienotebook/api/mapper/MovieMapper.java
@@ -4,7 +4,9 @@ import com.movienotebook.api.dto.movie.MovieResponseDto;
 import com.movienotebook.api.dto.movie.MovieShortResponseDto;
 import com.movienotebook.api.entity.Movie;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MovieMapper {
@@ -12,4 +14,10 @@ public interface MovieMapper {
 	MovieResponseDto toDto(Movie movie);
 	
 	MovieShortResponseDto toShortDto(Movie movie);
+	
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "reviews", ignore = true)
+	@Mapping(target = "ratings", ignore = true)
+	@Mapping(target = "averageRating", ignore = true)
+	void updateMovieFromExtracted(@MappingTarget Movie target, Movie source);
 }
