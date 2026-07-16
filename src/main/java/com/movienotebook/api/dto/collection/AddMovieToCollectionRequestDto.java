5bdf760fad5a41c68de65b72a555495458efe842
Maxim-Muzhikov <muzikov.maxim@gmail.com>
2026-07-16 15:36:36 +0400

    refactor: delete unused dto

--- src/main/java/com/movienotebook/api/dto/collection/AddMovieToCollectionRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.movienotebook.api.dto.collection;
-
-import jakarta.validation.constraints.NotNull;
-
-public record AddMovieToCollectionRequestDto(
-		@NotNull(message = "Идентификатор фильма не может быть пустым")
-		Long movieId
-) {
-}
