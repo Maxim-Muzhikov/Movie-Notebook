853da0dbce4fcda35c2a8423bf206574d1f8bd4d
Maxim-Muzhikov <muzikov.maxim@gmail.com>
2026-07-16 13:49:56 +0400

    feat: full change collection mapper

--- src/main/java/com/movienotebook/api/mapper/CollectionMapper.java
+++ src/main/java/com/movienotebook/api/mapper/CollectionMapper.java
@@ -1,32 +1,29 @@
 package com.movienotebook.api.mapper;
 
 import com.movienotebook.api.dto.collection.CollectionResponseDto;
-import com.movienotebook.api.dto.movie.MovieResponseDto;
+import com.movienotebook.api.dto.collection.CollectionWithMoviesResponseDto;
+import com.movienotebook.api.dto.movie.MovieShortResponseDto;
 import com.movienotebook.api.entity.Collection;
 import com.movienotebook.api.entity.CollectionMovie;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import java.util.List;
-import java.util.Set;
-
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public abstract class CollectionMapper {
+// LEARN @Mapper(..., uses = {...})
+@Mapper(componentModel = "spring", uses = {MovieMapper.class})
+public interface CollectionMapper {
+	// NOTE Этот маппинг нужен из-за особенностей совместимости Lombok и MapStruct
+	@Mapping(target = "isPublic", source = "public")
+	CollectionResponseDto toDto(Collection collection);
 	
-	protected final MovieMapper movieMapper;
+	@Mapping(target = "authorUsername", source = "user.username")
+	@Mapping(target = "movies", source = "collectionMovies")
+	@Mapping(target = "isPublic", source = "public")
+	CollectionWithMoviesResponseDto toWithMoviesDto(Collection collection);
 	
-	protected CollectionMapper(MovieMapper movieMapper) {
-		this.movieMapper = movieMapper;
-	}
-	
-	@Mapping(target = "movies", source = "collectionMovies", qualifiedByName = "mapCollectionMovies")
-	public abstract CollectionResponseDto toDto(Collection collection);
-	
-	protected List<MovieResponseDto> mapCollectionMovies(Set<CollectionMovie> collectionMovies) {
-		if (collectionMovies == null) {
-			return List.of();
-		}
-		return collectionMovies.stream()
-				.map(cm -> movieMapper.toDto(cm.getMovie()))
-				.toList();
-	}
+	// NOTE Вот что происходит в toMovieShortDto:
+	// MapStruct видит, что источник - это CollectionMovie, а цель - MovieShortResponseDto.
+	// Аннотация target = "." указывает: "возьми объект movie внутри CollectionMovie и замапь его свойства".
+	// А так как в uses указан MovieMapper, MapStruct сам сгенерирует вызов movieMapper.toShortDto()!
+	@Mapping(target = ".", source = "movie")
+	MovieShortResponseDto toMovieShortDto(CollectionMovie collectionMovie);
 }
