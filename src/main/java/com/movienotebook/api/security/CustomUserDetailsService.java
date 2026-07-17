54a8c63b72d655432c8968fcad1e56633882247b
Maxim-Muzhikov <muzikov.maxim@gmail.com>
2026-07-17 10:45:34 +0400

    fix: edit return type to custom user details

--- src/main/java/com/movienotebook/api/security/CustomUserDetailsService.java
+++ src/main/java/com/movienotebook/api/security/CustomUserDetailsService.java
@@ -21,7 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 	private final UserRepository userRepository;
 	
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 		User user = userRepository.findByUsername(username)
 				.orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
