25f7d0c7dd357a7deca9f87626dda1e2246903ae
Maxim-Muzhikov <muzikov.maxim@gmail.com>
2026-08-14 14:37:26 +0400

    refactor: move rating mapping to service

--- src/main/java/com/movienotebook/api/controller/RatingController.java
+++ src/main/java/com/movienotebook/api/controller/RatingController.java
@@ -2,7 +2,6 @@ package com.movienotebook.api.controller;
 
 import com.movienotebook.api.dto.rating.RatingRequestDto;
 import com.movienotebook.api.dto.rating.RatingResponseDto;
-import com.movienotebook.api.entity.User;
 import com.movienotebook.api.security.CustomUserDetails;
 import com.movienotebook.api.service.RatingService;
 import com.movienotebook.api.service.UserService;
@@ -10,10 +9,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/ratings")
@@ -27,9 +26,7 @@ public class RatingController {
 	public ResponseEntity<RatingResponseDto> rateMovie(
 			@Valid @RequestBody RatingRequestDto request,
 			@AuthenticationPrincipal CustomUserDetails userDetails) {
-		
-		BigDecimal newAvg = ratingService.addOrUpdateRating(request, userDetails);
-		
-		return ResponseEntity.ok(new RatingResponseDto(newAvg));
+			
+		return ResponseEntity.ok(ratingService.save(request, userDetails));
 	}
 }
